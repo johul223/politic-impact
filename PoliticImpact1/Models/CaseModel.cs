b6ab90796197e12593049320ef8a13abe1fb072d
Johannes Dahlgren <johda876@student.liu.se>
2012-11-06 13:37:06 +0100

    Revert "Changed name and added datavalidation"
    This reverts commit cee2c298c4ec4872cd151cefa254a2682f7555ac.

--- PoliticImpact1/Models/CaseModel.cs
+++ PoliticImpact1/Models/CaseModel.cs
@@ -16,29 +16,12 @@ namespace PoliticImpact1.Models
         /*
          * Variables from "Dropbox/Arkitektur/tables.doc"
          * */
-        [Required]
         public int ID { get; set; }
-
-        [Required]
         public long Owner { get; set; }
-
-        [Required]
-        [StringLength(100, ErrorMessage="{0} must be at least {2} characters long.", MinimumLength=5)]
         public string Title { get; set; }
-
-        [Required]
-        [StringLength(10000,ErrorMessage="You must have a description.", MinimumLength=1)]
         public string Text { get; set; }
-
-        [Required]
         public bool Published { get; set; }
-
-        [Required]
-        [DataType(DataType.DateTime)]
         public DateTime Created { get; set; }
-
-        [Required]
-        [DataType(DataType.DateTime)]
         public DateTime LastEdited { get; set; }
     }
 
