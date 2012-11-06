128371ed062c72524504378f228c46748a4c5ec7
johul223 <johul223@student.liu.se>
2012-11-07 00:16:02 +0100

    Minor changes in case model, add dbcontext

--- PoliticImpact1/Models/CaseModels.cs
+++ PoliticImpact1/Models/CaseModels.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 /*
  * Created by: Johannes Dahlgren
---
+++ PoliticImpact1/Views/Home/PoliticImpactDBContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PoliticImpact1.Views.Home
+{
+    public class PoliticImpactDBContext : DbContext
+    {
+        public DbSet<PoliticImpact1.Models.CaseModel> Cases { get; set; }
+    }
+}
