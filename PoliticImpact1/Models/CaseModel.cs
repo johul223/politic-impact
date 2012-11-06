070408b0d4ef885a11b1708d792d8d1657b34342
Johannes Dahlgren <johda876@student.liu.se>
2012-11-06 10:12:52 +0100

    Added CaseModel.cs
    Model for the cases

---
+++ PoliticImpact1/Models/CaseModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+/*
+ * Created by: Johannes Dahlgren
+ * Created at: 6/11 2012
+ */
+
+namespace PoliticImpact1.Models
+{
+    public class CaseModel
+    {
+        public int ID { get; set; }
+        public long Owner { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public bool Published { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastEdited { get; set; }
+    }
+
+}
