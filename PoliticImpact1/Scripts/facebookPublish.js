1f2a8cf53058e50c15c339fed4f43d0218d7d388
Semone Kallin Clarke <semoneclarke@gmail.com>
2012-11-06 17:47:45 +0100

    Facebook Share
    Javascripts for sharing on Facebook

---
+++ PoliticImpact1/Scripts/facebookPublish.js
@@ -0,0 +1,17 @@
+﻿function publishStory() {
+    FB.ui({
+        method: 'feed',
+        name: 'I\'m doing a test!', //This should be the subject of the case
+        //caption: 'Hope this works', 
+        description: 'Check out Facebook\'s developer site to start building.', //This should be the discription of the case
+        link: 'http://annasemone.se', //This should be the page I'm on, the page of the case
+        picture: 'http://annasemone.se/images/niklas2.JPG' // This should be the image of the case
+    },
+    function (response) {
+        console.log('publishStory response: ', response);
+    });
+    return false;
+}
+/*@Scripts.Render("~/Scripts/publishStory.js")
+@Scripts.Render("~/Scripts/facebookConnect.js")
+<a href="#" onclick="publishStory();">Publish feed story</a><br>*/
