8ecb2c14ef0122ea998e98fc49d8895064d9d005
johul223 <johul223@student.liu.se>
2012-11-06 10:42:26 +0100

    Created view to create case. Had to add CaseController to be able to view it
    Since I didnt have the model, I the validation of fields is not made.

---
+++ PoliticImpact1/Controllers/CaseController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PoliticImpact1.Controllers
+{
+    public class CaseController : Controller
+    {
+        //
+        // GET: /Default1/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
